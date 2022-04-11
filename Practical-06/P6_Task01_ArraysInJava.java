ace1b351a08fc416632ed37ea8ec4223fde9957e
ShagunSemwal <100461300+ShagunSemwal@users.noreply.github.com>
2022-04-11 18:29:27 +0530

    Add files via upload

---
+++ Practical-06/P6_Task01_ArraysInJava.java
@@ -0,0 +1,90 @@
+package Unit_01;
+
+/*
+ -Normally,an array is a collection of similar type of elements which has contiguous memory location
+ -Java array is an object which contains elements of a similar data type 
+ - we can store only a fixed set of element in a have array
+ -size limit issue:
+ 	-we can store only the fixed size of elements in the array.
+ 	-It doesn't grow its size at runtime.
+ 	-To solve this problem ,collection framework is used in java which grows automatically
+ 
+ -There are two types o f array
+ 	-single dimensional array
+ 	-multidimensional array
+ 	
+ -declare an array in java
+ 	-datatype arrayRefvar[];
+ 	OR
+ 	-datatype[] arrayRefVar;
+ 	OR
+ 	- datatype []arrayRefVar;
+ */
+
+public class P6_Task01_ArraysInJava {
+
+	public static void main(String[] args) {
+		
+		//Declaration
+		int array1[];
+		
+		int[] array2;
+		int []array3;
+		
+		//can not do it as we have not provide it any memory/space yet!
+		//array1[0] =1;
+		
+		//initialization, during intialization we have to provide the size of the array
+		array1= new int[6]; //{1,2,3,4};
+		
+		// Assigning values to the Java array
+		for (int i=0; i< array1.length; i++) // length is the property of array
+			array1[i] = i;
+		
+		int array4[]= {33,3,4,5}; //declaration,instantiation and initialization
+		
+		//passing anonymous array in a method
+		arrayAsParameter(new int[] {33,3,4,5});
+		
+		//Returning Array from the Method
+		int[] array5 =  arrayAsReturnType();
+		System.out.println(array5.toString());
+		
+		//ArrayIndexOutOfBoundsExeption
+		int arr[]= {50,60,70,80};
+		for(int i =0 ;i<= arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		//Multidimensional Array In Java
+		int [][] arr6= new int[3][3]; // 3 row 3 column
+		
+		for(int i=0, j=0; i<3 && j<3; i++ ,j++ ) {
+			System.out.println(arr6[i][j]);
+		}
+		
+		int counter=0;
+		for(int i=0,j=0;i<3 && j<3 ; i++, j++) {
+			counter++;
+			arr6[i][j]= counter;
+			
+		}
+		for(int i=0,j=0;i<3 && j<3 ; i++, j++) {
+			System.out.println(arr6[i][j]);
+		}
+		
+	}
+	
+	static void arrayAsParameter(int arr[]) { //declare empty array as parameter
+		for(int i=0; i< arr.length; i++)  //length is the property of array
+			System.out.println(arr[i]);
+	}
+	
+	// Returning Array from the Method
+	static int[] arrayAsReturnType() {
+		int array4[]= {33,3,4,5};
+		
+		return array4;
+	}
+
+}
